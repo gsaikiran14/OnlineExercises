df4aeff37ef818b06cc7477afa1785945cd8d905
Sai Kiran Gandu <gsaikiran941@gmail.com>
2019-07-30 13:31:32 +0530

    Add files via upload

---
+++ SockMerchant.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    count = 0
+    b=set()
+    for i in range(n):
+        a=ar[i]
+        if a in b:
+            b.remove(a)
+            count+=1
+        else:
+            b.add(a)
+    return count
+
+
+    
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
